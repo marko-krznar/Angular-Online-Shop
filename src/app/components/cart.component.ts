0a2b1cd3791bb54b920d1ae3e1abe7ea600fa669
marko <krznarm26@gmail.com>
2024-12-23 14:20:18 +0100

    Added empty cart element

--- src/app/components/cart.component.ts
+++ src/app/components/cart.component.ts
@@ -1,17 +1,31 @@
-import { Component, inject, effect } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CartService } from '../services/cart.service';
+import { RouterLink } from '@angular/router';
+import { PrimaryButtonComponent } from './primary-button.component';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [PrimaryButtonComponent, RouterLink],
   template: `
-    <div class="flex flex-col items-center justify-center p-4 rounded gap-4">
-      @for (product of cartService.cart(); track product.id) {
-      <div class="flex justify-between w-full">
-        <h1>{{ product.name }}</h1>
-        <span>{{ product.price }}</span>
-        <button (click)="cartService.removeFromCart(product.id)">Remove</button>
+    <div class="flex flex-col items-center justify-center gap-4 p-6">
+      @if (cartService.cart().length > 0) {
+      <div
+        class="flex flex-col items-center justify-center rounded gap-4 w-full"
+      >
+        @for (product of cartService.cart(); track product.id) {
+        <div class="flex justify-between w-full">
+          <h2>{{ product.name }}</h2>
+          <span>{{ product.price }}</span>
+          <button (click)="cartService.removeFromCart(product.id)">
+            Remove
+          </button>
+        </div>
+        }
       </div>
+      } @else {
+      <h2 class="text-xl font-bold">Empty Cart</h2>
+      <p>There are no products added in the cart.</p>
+      <app-primary-button label="Add Products to the Cart" routerLink="/" />
       }
     </div>
   `,
--- src/app/components/primary-button.component.ts
+++ src/app/components/primary-button.component.ts
@@ -5,7 +5,7 @@ import { Component, input, output } from '@angular/core';
   imports: [],
   template: `
     <button
-      class="bg-blue-500 text-white w-full border px-5 py-2 rounded-xl shadow-md hover:opacity-90"
+      class="bg-blue-500 text-white border px-5 py-2 rounded-xl shadow-md hover:opacity-90"
       (click)="btnClicked.emit()"
     >
       {{ label() }}
