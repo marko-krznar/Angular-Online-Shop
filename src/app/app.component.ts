a6bf03e784f7459a96b077aa681f3b54b80fb6be
marko <krznarm26@gmail.com>
2024-12-26 00:47:08 +0100

    Added angular material components library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './components/header.component';
 import { RouterOutlet } from '@angular/router';
+
+import { HeaderComponent } from './components/header.component';
 
 @Component({
 	selector: 'app-root',
